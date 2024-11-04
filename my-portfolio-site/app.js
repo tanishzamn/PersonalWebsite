324e7d906095ccbeac720ea4db7dc7c003d47343
tanishzamn <tanisha.zaman@ontariotechu.net>
2024-11-03 23:13:01 -0500

    pages

--- my-portfolio-site/app.js
+++ my-portfolio-site/app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// Set EJS as the templating engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Serve static files (like CSS and images) from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Define routes
+app.get('/', (req, res) => {
+  res.render('home');  // Renders the home.ejs file
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/about', (req, res) => {
+  res.render('about'); // Renders the about.ejs file
 });
 
-module.exports = app;
+app.get('/projects', (req, res) => {
+  res.render('projects'); // Renders the projects.ejs file
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact'); // Renders the contact.ejs file
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
