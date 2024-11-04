9bdbb981af85d6bd70ced04220c619caf954bdc8
tanishzamn <tanisha.zaman@ontariotechu.net>
2024-11-04 00:38:40 -0500

    pages

--- my-portfolio-site/app.js
+++ my-portfolio-site/app.js
@@ -1,33 +1,56 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-// Set EJS as the templating engine
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
-// Serve static files (like CSS and images) from the public directory
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes
-app.get('/', (req, res) => {
-  res.render('home');  // Renders the home.ejs file
+
+// Define your routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/about', (req, res) => {
-  res.render('about'); // Renders the about.ejs file
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get('/projects', (req, res) => {
-  res.render('projects'); // Renders the projects.ejs file
+module.exports = app;
+
+// Start the server if this file is run directly
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact'); // Renders the contact.ejs file
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Middleware setup
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
 });
