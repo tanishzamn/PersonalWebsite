ebf84e36975f0a6d103e88d5bd9d129f2410465b
tanishzamn <tanisha.zaman@ontariotechu.net>
2024-11-04 19:19:33 -0500

    adjustments

--- my-portfolio-site/app.js
+++ my-portfolio-site/app.js
@@ -1,15 +1,13 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
 
-let app = express();
+var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,19 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
-
-// Define your routes
+// Use the indexRouter for all main routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,16 +36,7 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 // Start the server if this file is run directly
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
   console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// Middleware setup
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use((req, res, next) => {
-  console.log(`Request URL: ${req.url}`);
-  next();
 });
--- my-portfolio-site/routes/index.js
+++ my-portfolio-site/routes/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-router.get('/', (req, res) => res.render('home', { title: 'Home' }));
-router.get('/about', (req, res) => res.render('about', { title: 'About Me' }));
-router.get('/projects', (req, res) => res.render('projects', { title: 'Projects' }));
-router.get('/contact', (req, res) => res.render('contact', { title: 'Contact Me' }));
+// Home Page
+router.get('/', function(req, res) { 
+    res.render('home', { title: 'Home' }); 
+});
+
+// About Me Page
+router.get('/about', function(req, res) { 
+    res.render('about', { title: 'About Me' }); 
+});
+
+// Projects Page
+router.get('/projects', function(req, res) { 
+    res.render('projects', { title: 'Projects' }); 
+});
+
+// Contact Me Page
+router.get('/contact', function(req, res) { 
+    res.render('contact', { title: 'Contact Me' }); 
+});
 
 module.exports = router;
