de047d79d5007a3c912a65286e7f5a646a982081
tanishzamn <tanisha.zaman@ontariotechu.net>
2024-11-05 23:22:18 -0500

    final changes

--- my-portfolio-site/app.js
+++ my-portfolio-site/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var createError = require('http-errors'); // Add this line to fix the "createError" issue
+var createError = require('http-errors'); 
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// View engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,20 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the indexRouter for all main routes
 app.use('/', indexRouter);
 
-// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404)); // Fix the "createError" issue
+  next(createError(404)); 
 });
 
-// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error'); // This will render error.ejs, ensure it exists
+  res.render('error'); 
 });
 
 // Start the server if this file is run directly
